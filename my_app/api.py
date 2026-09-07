fcdafadb39e1415cf40894691adde6d109e3a439
JamunadeviG <jamunadevig.2006@gmail.com>
2026-09-07 13:00:44 +0000

    Rate limit task completed

--- my_app/api.py
+++ my_app/api.py
@@ -214,3 +214,11 @@ def create_customer(name, phone, email):
     })
     doc.insert()
     return name
+
+@frappe.whitelist(allow_guest=True)
+@frappe.rate_limiter.rate_limit(limit=5)
+def limited_greeting():
+    logger = frappe.logger()
+    logger.info("Endpoint called.")
+
+    frappe.response["message"] = "Hello, Rate Limited World!"
